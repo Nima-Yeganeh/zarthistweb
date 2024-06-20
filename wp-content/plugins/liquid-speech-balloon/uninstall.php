f065705a4d60d5041d1a60a9922d5c070ca34c59
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-20 09:40:16 +0000

    done

--- wp-content/plugins/liquid-speech-balloon/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit();
-}
-delete_option( 'liquid_speech_balloon' );
-delete_option( 'liquid_speech_balloon_img' );
-delete_option( 'liquid_speech_balloon_name' );
-?>
